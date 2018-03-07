72a2c7be56af40167a2666f01127caf98e55c99d
Ray <rtrounday@gmail.com>
2018-03-06 21:29:28 -0800

    Added config

--- ChatterBot.py
+++ ChatterBot.py
@@ -6,14 +6,17 @@ import time
 
 # Twitter API Keys
 import os
-consumer_key = os.getenv("twitter_consumer_key")
-consumer_secret = os.getenv("twitter_consumer_secret")
-access_token = os.getenv("twitter_access_token")
-access_token_secret = os.getenv("twitter_access_token_secret")
+# consumer_key = os.getenv("twitter_consumer_key")
+# consumer_secret = os.getenv("twitter_consumer_secret")
+# access_token = os.getenv("twitter_access_token")
+# access_token_secret = os.getenv("twitter_access_token_secret")
+from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_LEVEL
+
+
 
 # Setup Tweepy API Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_LEVEL)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Create a function that tweets
